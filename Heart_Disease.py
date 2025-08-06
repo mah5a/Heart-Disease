5f8ce585125790cb3e41662cd5eea726fa8ea77c
mah5a <46025103+mah5a@users.noreply.github.com>
2025-08-05 23:26:30 -0700

    Data Wrangling_1 commit

--- Heart_Disease.py
+++ Heart_Disease.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 # from ucimlrepo import fetch_ucirepo
 #
 # # fetch dataset
@@ -14,10 +16,9 @@ import pandas as pd
 # # variable information
 # print(heart_disease.variables)
 
-with open(r"D:\mahsa\LEARNING\Data Analysis with Python\PRACTICE\heart+disease\new.data", "r") as file:
+with open(r"D:\mahsa\LEARNING\PRACTICE\new.data", "r") as file:
     for _ in range(5):
         print(file.readline())
-
 
 columns = [
     'id', 'ccf', 'age', 'sex', 'painloc', 'painexer', 'relrest', 'pncaden', 'cp',
@@ -31,9 +32,32 @@ columns = [
     'rcaprox', 'rcadist', 'lvx1', 'lvx2', 'lvx3', 'lvx4', 'lvf', 'cathef', 'junk', 'name'
 ]
 # df = pd.read_csv(r"D:\mahsa\LEARNING\Data Analysis with Python\PRACTICE\heart+disease/new.data", header=None, names=columns)
-df = pd.read_csv(r"D:\mahsa\LEARNING\Data Analysis with Python\PRACTICE\heart+disease/new.data",
-                 delim_whitespace=True,  # استفاده از فاصله به عنوان جداکننده
+df = pd.read_csv(r"D:\mahsa\LEARNING\PRACTICE\new.data",
+                 delim_whitespace=True,  # use space as seperator
                  header=None,
-                 names=columns)  # لیستی که قبلاً ساختیم
-print(df.head())
-print(df.shape)
+                 names=columns)
+# df.to_csv("D:\mahsa\LEARNING\PRACTICE\heart_disease.csv")
+
+# Inspect the Data
+# -----------------------------------------------------------------------
+pd.set_option('display.max_columns', None)
+print("\n-----------------------------\n Verify successful load with some randomly selected "
+      "records\n -----------------------------\n", df.sample(5))
+print("\n-----------------------------\n SHOW TOP 5 ROW OF DATA RECORDS: \n-----------------------------\n", df.head())
+
+print("\n ---------------------------\n STATISTICAL SUMMARY OF THE DATA\n------------------------------\n ",
+      df.describe())
+print("\n ---------------------------\n Dimensions of the data:\n------------------------------------\n", df.shape)
+print("\n ---------------------------\n Information about data\n ---------------------------\n", df.info)
+
+df1 = df.replace('?', np.NaN)
+df_empty_columns = df.dropna(axis=1, how='all')
+
+df_empty_rows = df_empty_columns.dropna(axis=0)
+print(df_empty_rows.isna().sum())
+print("\n ---------------------------\n Dimensions of the data:\n------------------------------------\n",
+      df_empty_rows.shape)
+df_invalid = df_empty_rows.replace('-9', np.NaN)
+df_clean = df_invalid.dropna(axis=0)
+print("\n ---------------------------\n Dimensions of the data:\n------------------------------------\n",
+      df_clean.shape)
