710674cc29830df387fa1cfa74e6267e3499faea
mah5a <46025103+mah5a@users.noreply.github.com>
2025-08-06 23:40:01 -0700

    Data cleaning test2

--- SRC/Heart_Disease.py
+++ SRC/Heart_Disease.py
@@ -66,6 +66,7 @@ print("\n ---------------------------\n Dimensions of the data:\n---------------
 
 df_cleaned.to_csv("D:\mahsa\LEARNING\PRACTICE\heart_disease_cleaned.csv")
 sns.boxplot(x=df['painexer'])
+sns.boxplot(x=df['painexer'])
 
 
 plt.show()
